82503b1c898fdaf9ccd7f00a13cc2eaf40c9635c
HEjOliverHej <oliver.bafverholm@elev.ga.lbs.se>
2026-05-04 12:02:52 +0200

    added admin ViewModel

--- betterschoolsoft/obj/Debug/net10.0-maccatalyst/maccatalyst-x64/betterschoolsoft.AssemblyInfo.cs
+++ betterschoolsoft/obj/Debug/net10.0-maccatalyst/maccatalyst-x64/betterschoolsoft.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("betterschoolsoft")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9e6994ebd99ccf9fb732c2344c1ca5a49de318d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3ac9c3af61c114c19bfae98fe46d758b411f0b29")]
 [assembly: System.Reflection.AssemblyProductAttribute("betterschoolsoft")]
 [assembly: System.Reflection.AssemblyTitleAttribute("betterschoolsoft")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
